309245846e3aba98a7ecbc90c82d33ceaf0c9383
angelica-marie-matthews <angelica.marie.matthews@outlook.com>
2026-06-08 17:21:47 -0400

    grammatical errors

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,17 +15,17 @@ public class Main {
         }
     }
 }
-int interOperandA=12
-        int interOperandB=40
-                int intSum=20
-                        int intProduct=15
-                                int intDifference=5
-                                        int intQuotient=100
+int interOperandA=12;
+        int interOperandB=40;
+                int intSum=20;
+                        int intProduct=15;
+                                int intDifference=5;
+                                        int intQuotient=100;
                                                 int intModulo=10;
-int doubleOperandA=75.8
-        int doubleOperandB=6
-                int doubleSum=1
-                        int doubleProduct=11
-                                int doubleDifference=7
-                                        int doubleQuotient=190
+int doubleOperandA=75.8;
+        int doubleOperandB=6;
+                int doubleSum=1;
+                        int doubleProduct=11;
+                                int doubleDifference=7;
+                                        int doubleQuotient=190;
 
