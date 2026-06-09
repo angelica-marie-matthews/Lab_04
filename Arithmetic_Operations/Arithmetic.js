f82276147a3b1270668d38d59365ed984def5a15
angelica-marie-matthews <angelica.marie.matthews@outlook.com>
2026-06-09 07:49:22 -0400

    commit the beginning of the arithmetic assignment

---
+++ Arithmetic_Operations/Arithmetic.js
@@ -0,0 +1,21 @@
+INT; VARIABLES
+
+int; intOperandA= 12;
+int; intOperandB =40;
+int; intSum=20;
+int; intProduct= 15;
+int; intDifference= 5;
+int; intQuotinet= 100;
+int; intModulo=10;
+
+DOUBLE; VALUE; VARIABLES
+
+doubleOperandA = 75.8;
+doubleOperandB=6;
+doubleSum= 1;
+doubleProduct=11;
+doubleDifference= 7;
+doubleQuotient= 190;
+
+intSum = intOperandA+intOperandB;
+System.out.printIn ("The sum using ints of"+ intOperandA+"and"+ intOperandB+ "is"+intSum);
